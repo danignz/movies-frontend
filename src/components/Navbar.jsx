f5680a91977d575c91a86bcf00e041cf72242e34
Daniel Gonzalez <danielgnzch@gmail.com>
2022-08-27 12:52:21 +0200

    Iteration 1 done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
     <div>
-      {/* Should use react-router-dom NavLinks to browse through the different pages */}
+      <ul>
+        <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/">Home</NavLink></li>
+        <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/new">New</NavLink></li>
+        <li><button onClick={() => navigate(-1)}>Go back</button></li>
+      </ul>
     </div>
   )
 }
